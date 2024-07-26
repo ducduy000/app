d87c1adef51e1e161672d50232873c1a05a3a3da
ducduy000 <167493730+ducduy000@users.noreply.github.com>
2024-07-26 10:42:31 +0700

    Add files via upload

---
+++ lib/src/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const tPrimaryColor = Color(0xFFFFE400);
+const tSecondaryColor = Color(0xff272727);
+const tAccentCOlor = Color(0xFF001BFF);
+
+const tCardBgcolor = Color(0xFFF7F6F1);
+const tWhiteColor = Color(0xffffffff);
+const tDarkColor = Color(0xFF000000);
